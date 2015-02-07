106750bee73b6061b6fc1f4b4465f45bb2e76e2c
Santosh Kumar <santosh79@gmail.com>
2015-02-07 15:48:53 +0000

    minor cleanup

--- sicp_in_elixir/lib/datastructures_using_closures.ex
+++ sicp_in_elixir/lib/datastructures_using_closures.ex
@@ -1,6 +1,6 @@
 defmodule DS do
   def test do
-    s                   = stack [3]
+    s                   = stack
     with_ten_and_twenty = s |> push(10) |> push(20)
     twenty              = with_ten_and_twenty |> pop |> Enum.at(1)
     ten                 = with_ten_and_twenty |> pop |> Enum.at(0) |> pop |> Enum.at(1)
@@ -23,6 +23,6 @@ defmodule DS do
   end
 
   def push(stack, item), do: stack.(:push, item)
-  def pop(stack), do: stack.(:pop, nil)
+  def pop(stack), do: stack.(:pop, :void)
 end
 
