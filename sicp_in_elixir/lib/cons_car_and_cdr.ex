f2a09cb21fb5e7a6105219b81327190582b3aa3e
Santosh Kumar <santosh79@gmail.com>
2015-02-09 21:55:57 +0000

    list car and cdr handling bumping off the end of the list

--- sicp_in_elixir/lib/cons_car_and_cdr.ex
+++ sicp_in_elixir/lib/cons_car_and_cdr.ex
@@ -10,5 +10,19 @@
 
     def car(x), do: x.(:car)
     def cdr(x), do: x.(:cdr)
+
+    def list_ref(items, n) do
+      list_ref items, n, 0
+    end
+    defp list_ref(l, n, n), do: car(l)
+    defp list_ref(nil, _, _), do: nil
+    defp list_ref(l, n, cnt) do
+      list_ref cdr(l), n, (cnt + 1)
+    end
+
+    def list([h]), do: cons(h, nil)
+    def list([h|t]) do
+      cons(h, list(t))
+    end
   end
 
--- sicp_in_elixir/test/cons_car_and_cdr_test.exs
+++ sicp_in_elixir/test/cons_car_and_cdr_test.exs
@@ -3,9 +3,20 @@ defmodule ConsCarAndCdrTest do
   import ConsCarAndCdr
 
   test "simple test" do
-    a = cons(10, 20)
+    a = cons 10, 20
     assert car(a) == 10
     assert cdr(a) == 20
+  end
+
+  test "list" do
+    one_thru_four = 1..4 |> Enum.to_list |> list
+    assert car(cdr(one_thru_four)) == 2
+  end
+
+  test "list_ref" do
+    one_thru_four = 1..4 |> Enum.to_list |> list
+    assert list_ref(one_thru_four, 2) == 3
+    assert list_ref(one_thru_four, 20) == nil
   end
 
 end
